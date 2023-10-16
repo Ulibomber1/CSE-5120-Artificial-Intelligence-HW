98df622217bfc61bd0d733efaca6778ba39ebcd5
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2023-10-16 07:28:34 -0700

    First Prototype Pass

--- multiAgents.py
+++ multiAgents.py
@@ -18,22 +18,65 @@ def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=fl
 
 	# return value, best_move
 
+"""
+YOUR CODE HERE TO CALL NEGAMAX FUNCTION. REMEMBER THE RETURN OF THE NEGAMAX SHOULD BE THE OPPOSITE OF THE CALLING
+PLAYER WHICH CAN BE DONE USING -NEGAMAX(). THE REST OF YOUR CODE SHOULD BE THE SAME AS MINIMAX FUNCTION.
+YOU ALSO DO NOT NEED TO TRACK WHICH PLAYER HAS CALLED THE FUNCTION AND SHOULD NOT CHECK IF THE CURRENT MOVE
+IS FOR MINIMAX PLAYER OR NEGAMAX PLAYER
+RETURN THE FOLLOWING TWO ITEMS
+1. VALUE
+2. BEST_MOVE
+    
+THE LINE TO RETURN THESE TWO IS COMMENTED BELOW WHICH YOU CAN USE
+    
+"""
 def negamax(game_status: GameStatus, depth: int, turn_multiplier: int, alpha=float('-inf'), beta=float('inf')):
 	terminal = game_status.is_terminal()
 	if (depth==0) or (terminal):
-		scores = game_status.get_negamax_scores(terminal)
-		return scores, None
+		scores = turn_multiplier * game_status.get_negamax_scores(terminal)
+		return (scores, None)
 
-	"""
-    YOUR CODE HERE TO CALL NEGAMAX FUNCTION. REMEMBER THE RETURN OF THE NEGAMAX SHOULD BE THE OPPOSITE OF THE CALLING
-    PLAYER WHICH CAN BE DONE USING -NEGAMAX(). THE REST OF YOUR CODE SHOULD BE THE SAME AS MINIMAX FUNCTION.
-    YOU ALSO DO NOT NEED TO TRACK WHICH PLAYER HAS CALLED THE FUNCTION AND SHOULD NOT CHECK IF THE CURRENT MOVE
-    IS FOR MINIMAX PLAYER OR NEGAMAX PLAYER
-    RETURN THE FOLLOWING TWO ITEMS
-    1. VALUE
-    2. BEST_MOVE
-    
-    THE LINE TO RETURN THESE TWO IS COMMENTED BELOW WHICH YOU CAN USE
-    
-    """
-    #return value, best_move
+	moves: list = game_status.get_moves()
+
+	value = alpha
+	best_move = None
+	for move in moves:
+		newGameStatus = game_status.get_new_state(move)
+		negaMaxReturn, next_best_move = negamax(newGameStatus, depth - 1, -turn_multiplier, -beta, -alpha)
+
+		if -negaMaxReturn > value: # if negamax returns a greater value, set it as the new value
+			value = -negaMaxReturn
+			best_move = move
+		alpha = max(alpha, value) # if the value is greater than alpha, the alpha becomes equal to the value
+		if alpha >= beta:
+			break # prune the branch that will never be considered
+
+	return value, best_move
+
+
+boardState = [[ 0, 0, 0, 0], 
+			  [-1, 0, 1, 0],
+			  [ 1, 1,-1, 0],
+			  [ 0, 0, 0, 0]]
+
+gameStatus = GameStatus(boardState, False)
+
+value , move = negamax(gameStatus, 2, -1)
+print("Value:" + str(value))
+print("Best Move:" + str(move))
+
+value , move = negamax(gameStatus, 3, -1)
+print("Value:" + str(value))
+print("Best Move:" + str(move))
+
+value , move = negamax(gameStatus, 4, -1)
+print("Value:" + str(value))
+print("Best Move:" + str(move))
+
+value , move = negamax(gameStatus, 5, -1)
+print("Value:" + str(value))
+print("Best Move:" + str(move))
+
+value , move = negamax(gameStatus, 6, -1)
+print("Value:" + str(value))
+print("Best Move:" + str(move))
